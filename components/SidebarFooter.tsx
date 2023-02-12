66c8da620a1c05c3d69cbf058397a0dfdf789a99
cris <ctovar@abexuspr.com>
2023-02-12 10:20:20 -0500

    📹 (1:09:03) Discover>SidebarFooter

--- components/SidebarFooter.tsx
+++ components/SidebarFooter.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+
+const List = ({ items, mt }: { items: string[], mt: boolean }) => {
+  return (
+    <div className={`flex flex-wrap gap-2 ${(mt) && 'mt-5'}`}>
+      {items.map((item) => {
+        return (
+          <p key={item} className="text-gray-400 text-sm hover:underline cursor-pointer">
+            {item}
+          </p>
+        )
+      })}
+
+    </div>
+  )
+}
 
 const SidebarFooter = () => {
   return (
-    <div>SidebarFooter</div>
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} mt={false} />
+      <List items={footerList2} mt={false} />
+      <List items={footerList3} mt={false} />
+      <p className="text-gray-400 text-sm mt-5">2023 JSM TikTik</p>
+    </div>
   )
 }
 
