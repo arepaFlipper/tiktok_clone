160aad10f8d252e87bc0c35f20040e3ec207675d
cris <ctovar@abexuspr.com>
2023-02-12 17:08:58 -0500

    🍏 (14:40) main video view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-
+import axios from 'axios';
 
 const Home: NextPage = () => {
   return (
@@ -7,6 +7,16 @@ const Home: NextPage = () => {
       Hello world
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`);
+  console.log(`🍟%cindex.tsx:14 - response`, 'font-weight:bold; background:#44bb00;color:#fff;'); //DELETEME
+  console.log(response.data.name); // DELETEME
+
+  return {
+    props: {}
+  }
 }
 
 export default Home;
