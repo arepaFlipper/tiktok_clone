d676c6c592d2f4163fee8efa21b70abf3bb021d0
cris <ctovar@abexuspr.com>
2023-02-12 07:09:02 -0500

    ⛳ (53:38) Sidebar>GoogleOAuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import { Navbar, Sidebar } from "../components";
 import '../styles/globals.css';
@@ -13,17 +14,19 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     return null
   }
   return (
-    <div>
-      <Navbar />
-      <div className='flex gap-6 md:gap-20'>
-        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          <Sidebar />
-        </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-          <Component {...pageProps} />
+    <GoogleOAuthProvider clientId={``}>
+      <div>
+        <Navbar />
+        <div className='flex gap-6 md:gap-20'>
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   )
 }
 
