bd3be48240f0f4c2def814433123e52ed0201655
cris <ctovar@abexuspr.com>
2023-02-12 04:50:15 -0500

    🥓 start the components file

---
+++ components/index.ts
@@ -0,0 +1,8 @@
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+export {
+  Sidebar,
+  Navbar,
+}
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
 
+import { Navbar, Sidebar } from "../components";
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -11,7 +12,19 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) {
     return null
   }
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <div className='flex gap-6 md:gap-20'>
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyApp;
