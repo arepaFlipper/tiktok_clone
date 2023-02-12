d7ce9bc36ae6d6cdd8570aaf9bc708dfd21b70eb
cris <ctovar@abexuspr.com>
2023-02-12 02:16:13 -0500

    ☠️  (17:10) fix postedBy import issue

--- sanity-backend/schemas/index.ts
+++ sanity-backend/schemas/index.ts
@@ -1,4 +1,5 @@
 import post from './post';
+import postedBy from './postedBy';
 import user from './user';
 import comment from './comment';
-export const schemaTypes = [post, user, comment]
+export const schemaTypes = [post, postedBy, user, comment]
