bfc3c1848948672a14f2a571c77e8943fdb59290
cris <ctovar@abexuspr.com>
2023-02-12 05:16:10 -0500

    ⏭️  (36:46) add Navbar imports

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from "next/router";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Link href="/">
+        <div>
+          <Image
+            className="cursor-pointer"
+            alt="TikTik"
+            layout="responsive"
+            src={Logo}
+          />
+        </div>
+      </Link>
+    </div>
   )
 }
 
