09dfeb19dda8a553598867fca4503c94fe1c37d3
cris <ctovar@abexuspr.com>
2023-02-14 18:41:15 -0500

    ⌛ (2:51:22)

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -29,8 +29,8 @@ const upload = () => {
     }
   }
   return (
-    <div className="flex w-full h-full">
-      <div className="bg-white rounded-lg">
+    <div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-(#F8F8F8) justify-center">
+      <div className="bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -38,35 +38,54 @@ const upload = () => {
           </div>
           <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
             {isLoading ? (<p>Uploading...</p>) : (
-              <label className="cursor-pointer">
-                <div className="flex flex-col items-center justify-center h-full">
-                  <div className="flex flex-col items-center justify-center ">
-                    <p className="fotn-bold text-xl">
-                      <FaCloudUploadAlt
-                        className="text-gray-300 text-6xl"
-                      />
-                    </p>
-                    <p className="text-xl font-semibold">
-                      Upload Video
-                    </p>
+              <div>
+                {videoAsset ? (
+                  <div>
+                    <video src={videoAsset.url} loop controls className="rounded-xl h-[450px] mt-16 bg-black" />
                   </div>
-                  <p className="text-gray-400 text-center mt-10 text-sm leading-10">
-                    MP4 or WebM or ogg<br />
-                    720x1280 or higher<br />
-                    Up to 10 minutes<br />
-                    Less than 2GB
-                  </p>
-                  <p className="bg-[#F51997] text-center mt-10 rounded text-white text-md font-medium p-2 w-52 outline-none">Select File</p>
-                </div>
-                <input
-                  type="file"
-                  name="upload-video"
-                  onChange={uploadVideo}
-                  className="w-0 h-0"
-                />
-              </label>
+                ) : (
+                  <label className="cursor-pointer">
+                    <div className="flex flex-col items-center justify-center h-full">
+                      <div className="flex flex-col items-center justify-center ">
+                        <p className="fotn-bold text-xl">
+                          <FaCloudUploadAlt
+                            className="text-gray-300 text-6xl"
+                          />
+                        </p>
+                        <p className="text-xl font-semibold">
+                          Upload Video
+                        </p>
+                      </div>
+                      <p className="text-gray-400 text-center mt-10 text-sm leading-10">
+                        MP4 or WebM or ogg<br />
+                        720x1280 or higher<br />
+                        Up to 10 minutes<br />
+                        Less than 2GB
+                      </p>
+                      <p className="bg-[#F51997] text-center mt-10 rounded text-white text-md font-medium p-2 w-52 outline-none">Select File</p>
+                    </div>
+                    <input
+                      type="file"
+                      name="upload-video"
+                      onChange={uploadVideo}
+                      className="w-0 h-0"
+                    />
+                  </label>
+                )}
+              </div>
+            )}
+            {wrongFileType && (
+              <p className="text-center text-xl text-red-400 font-semibold mt-4 w-[250px]">Please select a video file</p>
             )}
           </div>
+        </div>
+        <div className="flex flex-col gap-3 pb-10">
+          <label className="text-md font-medium">Caption</label>
+          <input
+            type="text"
+            value=""
+            onChange={() => { }}
+          />
         </div>
       </div>
     </div>
