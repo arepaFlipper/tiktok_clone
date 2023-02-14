ec8a023685f52b3c440b0c33dff2742b40eecf58
cris <ctovar@abexuspr.com>
2023-02-14 15:13:31 -0500

    🍡 (1:54:52)

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -74,7 +74,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
               />
             </Link>
             {isHover && (
-              <div>
+              <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3">
                 {playing ? (
                   <button onClick={onVideoPress}>
                     <BsFillPauseFill
@@ -89,13 +89,13 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                   </button>
                 )}
                 {isVideoMuted ? (
-                  <button>
+                  <button onClick={() => setIsVideoMuted(false)}>
                     <HiVolumeOff
                       className="text-black text-2xl lg:text-4xl"
                     />
                   </button>
                 ) : (
-                  <button>
+                  <button onClick={() => setIsVideoMuted(true)}>
                     <HiVolumeUp
                       className="text-black text-2xl lg:text-4xl"
                     />
