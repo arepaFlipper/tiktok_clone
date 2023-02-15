d560e3f8eab41c27a81b55ce861e655445261832
cris <ctovar@abexuspr.com>
2023-02-14 19:04:57 -0500

    ™️  (2:57:41)

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -31,7 +31,7 @@ const upload = () => {
   }
   return (
     <div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-(#F8F8F8) justify-center">
-      <div className="bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6">
+      <div className="bg-white rounded-lg xl:h-[80vh] w-[80%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -103,6 +103,22 @@ const upload = () => {
             })}
 
           </select>
+          <div className="flex gap-6 mt-10">
+            <button
+              onClick={() => { }}
+              type="button"
+              className="border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+            >
+              Discard
+            </button>
+            <button
+              onClick={() => { }}
+              type="button"
+              className="bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+            >
+              Post
+            </button>
+          </div>
         </div>
       </div>
     </div>
