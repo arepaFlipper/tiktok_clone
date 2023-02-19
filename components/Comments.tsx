dd1b07c915f6a25d09ed2979c2e08ab695a9895b
cris <ctovar@abexuspr.com>
2023-02-19 07:49:03 -0500

    ↔️ (1:35:00) Fetch allUsers

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch, SetStateAction } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { GoVerified } from 'react-icons/go';
-
+import { IUser } from "@/types";
 import useAuthStore from '@/store/authStore';
 import NoResults from './NoResults';
 
@@ -22,14 +22,55 @@ interface IComment {
 }
 
 const Comments = ({ isPostingComment, comment, setComment, addComment, comments }: IProps) => {
-  const { userProfile } = useAuthStore();
+  const { userProfile, allUsers } = useAuthStore();
   const handleChange = ({ value }: { value: string }) => {
-    setComment(value.trim());
+    setComment(value);
   }
   return (
     <div className="border-t-2 border-gray-200 pt-5 px-10 bg-(#F8F8F8] border-b-2 lg:pb-0 pb-[100px])">
       <div className="overflow-scroll lg:h[475px]">
-        {comments?.length ? (<div>videos</div>) : (<NoResults text="No comments yet!" />)}
+        {comments?.length
+          ? (
+            <div>
+              {comments?.length ? (
+                comments.map((item, idx) => {
+                  return (
+                    <>
+                      {allUsers.map((user: IUser) => {
+                        return ((user._id === (item.postedBy._id || item.postedBy._ref)) && (
+                          <div className="p-2 items-center" key={idx}>
+                            <Link href={`/profile/${user._id}`}>
+                              <div className="w-8 h-8">
+                                <Image
+                                  src={user.image}
+                                  width={34}
+                                  height={34}
+                                  className="rounded-full"
+                                  alt="user profile"
+                                />
+                              </div>
+                              <div className="hidden xl:block">
+                                <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                                  {user.userName.replaceAll(' ', '')}
+                                  <GoVerified className="text-blue-400" />
+                                </p>
+                                <p className="capitalize text-gray-400 text-xs">
+                                  {user.userName}
+                                </p>
+                              </div>
+                            </Link>
+                            <div>
+                              <p>{item.comment}</p>
+                            </div>
+                          </div>
+                        ))
+                      })}
+                    </>
+                  )
+                })
+              ) : <></>}
+            </div>
+          ) : (<NoResults text="No comments yet!" />)}
       </div>
       {userProfile && (
         <div className="absolute bottom-0 left-0 pb-6 px-2 md:px-10">
