aec0d56ec7222386c89a5f28999b4156c161cbe9
cris <ctovar@abexuspr.com>
2023-02-19 07:28:04 -0500

    📄 (1:30:25)

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -18,7 +18,26 @@ const SuggestedAccounts = () => {
         {allUsers.slice(0, 6).map((user: IUser) => {
           return (
             <Link href={`/profile/${user._id}`} key={user._id}>
-
+              <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold founded">
+                <div className="w-8 h-8">
+                  <Image
+                    src={user.image}
+                    width={34}
+                    height={34}
+                    className="rounded-full"
+                    alt="user profile"
+                  />
+                </div>
+                <div className="hidden xl:block">
+                  <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                    {user.userName.replaceAll(' ', '')}
+                    <GoVerified className="text-blue-400" />
+                  </p>
+                  <p className="capitalize text-gray-400 text-xs">
+                    {user.userName}
+                  </p>
+                </div>
+              </div>
             </Link>
           )
         })}
