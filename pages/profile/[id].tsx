7d4c5066c043ce38edc2d874aee56d6a205bbb1c
cris <ctovar@abexuspr.com>
2023-02-19 19:13:02 -0500

    ✂️ (1:45:) Profile Page

---
+++ pages/profile/[id].tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { GoVerified } from "react-icons/go";
+import axios from "axios";
+
+import { VideoCard, NoResults } from "@/components";
+import { IUser, Video } from "@/types";
+import { BASE_URL } from "@/utils";
+
+interface IProps {
+  data: {
+    user: IUser,
+    userVideos: Video[],
+    userLikedVideos: Video[],
+  }
+}
+
+const Profile = ({ data }: IProps) => {
+  return (
+    <div>Profile</div>
+  )
+};
+
+export const getServerSideProps = async ({ params: { id } }: { params: { id: string } }) => {
+  const res = await axios.get(`${BASE_URL}/api/profile/${id}`);
+
+  return {
+    props: { data: res.data }
+  }
+}
+
+export default Profile
