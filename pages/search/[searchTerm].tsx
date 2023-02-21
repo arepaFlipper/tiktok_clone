ab151400fb45732f8c1da39f544ed1b8e77ff998
cris <ctovar@abexuspr.com>
2023-02-21 06:51:40 -0500

    👧 (2:13:11) Search

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -11,9 +11,17 @@ import { BASE_URL } from "@/utils";
 import useAuthStore from "@/store/authStore";
 
 
-const Search = () => {
+const Search = ({ videos }: { videos: Video[] }) => {
+  const [isAccounts, setIsAccounts] = useState(false);
+  const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+  const isVideos = (!isAccounts) ? 'border-b-2 border-black' : 'text-gray-400';
   return (
-    <div></div>
+    <div className="w-full">
+      <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
+        <p className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`} onClick={() => setIsAccounts(true)}>Accounts</p>
+        <p className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`} onClick={() => setIsAccounts(false)}>Videos</p>
+      </div>
+    </div>
   )
 }
 
@@ -21,7 +29,7 @@ export const getServerSideProps = async ({ params: { searchTerm } }: { params: {
   const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
 
   return {
-    props: { data: res.data }
+    props: { videos: res.data }
   }
 }
 
