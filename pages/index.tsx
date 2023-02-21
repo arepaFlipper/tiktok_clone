b70183409e6c98e5ac6d40ed951251d9c2d8962a
cris <ctovar@abexuspr.com>
2023-02-21 08:05:33 -0500

    🚌 (2:30:10) Categories

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ interface IProps {
   videos: Video[];
 }
 
-const Home: NextPage = ({ videos }) => {
+const Home = ({ videos }: IProps) => {
   return (
     <div className='flex flex-col gap-10 videos h-full'>
       {videos.length ? (
@@ -22,12 +22,17 @@ const Home: NextPage = ({ videos }) => {
   )
 }
 
-export const getServerSideProps = async () => {
-  const { data, } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({ query: { topic } }: { query: { topic: string } }) => {
+  let response = null
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
 
   return {
     props: {
-      videos: data,
+      videos: response.data,
     }
   }
 }
