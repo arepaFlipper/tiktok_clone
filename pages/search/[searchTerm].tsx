d6233c595a803f1f96a4c775c37c392b02d018dd
cris <ctovar@abexuspr.com>
2023-02-21 07:25:39 -0500

    🌝 (2:17:13) Search

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -13,14 +13,29 @@ import useAuthStore from "@/store/authStore";
 
 const Search = ({ videos }: { videos: Video[] }) => {
   const [isAccounts, setIsAccounts] = useState(false);
+  const router = useRouter();
+  const { searchTerm } = router.query;
   const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
   const isVideos = (!isAccounts) ? 'border-b-2 border-black' : 'text-gray-400';
   return (
     <div className="w-full">
       <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
         <p className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`} onClick={() => setIsAccounts(true)}>Accounts</p>
-        <p className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`} onClick={() => setIsAccounts(false)}>Videos</p>
+        <p className={`text-xl font-semibold cursor-pointer mt-2 ${isVideos}`} onClick={() => setIsAccounts(false)}>Videos</p>
       </div>
+      {isAccounts ? (
+        <div>ACCOUNTS</div>
+      ) : (
+        <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
+          {(videos.length) ?
+            (videos.map((video, idx) => {
+              return (
+                <VideoCard post={video} key={idx} />
+              )
+            })) : (<NoResults text={`No video results for ${searchTerm}`} />)
+          }
+        </div>
+      )}
     </div>
   )
 }
