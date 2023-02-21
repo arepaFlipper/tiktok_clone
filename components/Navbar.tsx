5fe47250d8b6081d62b908d6b838df9bac80253e
cris <ctovar@abexuspr.com>
2023-02-21 06:20:42 -0500

    🚇 (2:06:06) Search on Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from "next/router";
@@ -10,16 +10,17 @@ import Logo from '../utils/tiktik-logo.png'
 import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
 
-interface IProps {
-  userProfile: {
-    image: any;
-  },
-  addUser: any;
-  removeUser: any;
-}
 
 const Navbar = () => {
-  const { userProfile, addUser, removeUser }: IProps = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
+  const [SearchValue, setSearchValue] = useState("");
+  const router = useRouter();
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (SearchValue) {
+      router.push(`/search/${SearchValue}`);
+    }
+  }
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -32,7 +33,23 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>SEARCH</div>
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            type="text"
+            value={SearchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search accounts and videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0"
+          />
+          <button onClick={handleSearch} className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400">
+            <BiSearch />
+          </button>
+        </form>
+      </div>
       <div>{userProfile ? (
         <div className="flex gap-5 md:gap-10">
           <Link href="/upload">
