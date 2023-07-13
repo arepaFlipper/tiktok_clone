b2dcd3b62112a8b48e72f98015816084142c3a61
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-13 16:34:55 -0500

    👁️ set environment repo patched

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+import axios from "axios";
+import { Video } from "@/types";
 
-const detail = () => {
+interface IProps {
+  postDetails: Video,
+}
+
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const videoRef = useRef(null);
+  if (!post) return null;
   return (
-    <div>Detail</div>
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
+        <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p>
+            <MdOutlineCancel className="text-white text-[35px]" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              ref={videoRef}
+              loop
+              onClick={() => { }}
+              src={post.video.asset.url}
+              className="h-full cursor-pointer"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default detail
+export default Detail
+
+export const getServerSideProps = async ({ params: { id } }: { params: { id: string } }) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+  return {
+    props: { postDetails: data }
+  }
+
+}
