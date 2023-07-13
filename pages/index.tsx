a21f00044966ee806c6baf7beb1dbbd8f64cc6dc
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-13 16:50:55 -0500

    🪁 patch

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,6 @@ const Home: NextPage = () => {
 
 export const getServerSideProps = async () => {
   const response = await axios.get(`http://localhost:3000/api/post`);
-  console.log(`🍟%cindex.tsx:14 - response`, 'font-weight:bold; background:#44bb00;color:#fff;'); //DELETEME
-  console.log(response.data.name); // DELETEME
 
   return {
     props: {}
